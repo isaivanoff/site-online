c5b631f76cd1f623a4b8ad14109a0cfeebf067bf
Isabela Ivanoff <isabela.ivanoff@gmail.com>
2024-05-30 18:26:27 -0300

    texto alterado

--- site-online/src/App.js
+++ site-online/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> first project in react.
+          Edit <code>src/App.js</code> alteração.
         </p>
         <a
           className="App-link"
